99e5269572a346786c4acb7aa9e2c8b60730ad08
Luis Josue Suastegui Nranjo <luis.suasteguin@ug.edu.ec>
2024-11-16 23:27:49 -0500

    agg product

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,14 @@ export class HeaderComponent  implements OnInit {
 
   @Input() title!: string;
   @Input() backButton!: string;
+  @Input() isModal!: boolean;
 
-  constructor() { }
+
+  utilsSvc = inject(UtilsService);
+
+  dismissModal(){
+    this.utilsSvc.dismissModal();
+  }
 
   ngOnInit() {}
 
